f354dd692fde9b546e80f29d7b767986bc76da76
Phuc134 <67618897+Phuc134@users.noreply.github.com>
2023-01-30 11:01:36 +0700

    .

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,31 @@
+import { FiSearch } from "react-icons/fi";
+import { Link } from "react-router-dom";
+import "./Header.css";
+export default function Header() {
+  return (
+    <div className="header">
+      <div className="leftHeader">
+        <Link to="/" className="itemLeftHeader">
+          Trang chủ
+        </Link>
+        <Link to="/profile" className="itemLeftHeader">
+          Profile
+        </Link>
+        <Link to="/cart" className="itemLeftHeader">
+          Giỏ hàng
+        </Link>
+      </div>
+      <div className="rightHeader">
+        <input
+          className="itemRightHeader"
+          type="text"
+          placeholder="#Tim kiem"
+        ></input>
+        <FiSearch
+          className="itemRightHeader icon"
+          style={{ fontSize: "25px", padding: "3 5px" }}
+        />
+      </div>
+    </div>
+  );
+}
